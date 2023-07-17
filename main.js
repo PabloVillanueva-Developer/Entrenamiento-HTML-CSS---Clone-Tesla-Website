119fd4055ad53486c0051302bbfff42b6757131c
Pablo Villanueva <flordemadera@gmail.com>
2023-07-17 00:09:58 -0300

    menu Hamb terminado

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
+/* APERTURA MENU HAMB */
 const buttonMenu = document.getElementById('buttonMenu')
-
-
 buttonMenu.addEventListener('click', function () {
 const menuHamb = document.getElementById('menuHamb')
+const videoDesenfoque = document.getElementById('videoBlur')
+menuHamb.style.display = 'flex'
+
+setTimeout(function () {
 menuHamb.classList.remove('header__navHamb__ul')
 menuHamb.classList.add('header__navHamb__ul--b')
-console.log(menuHamb)
+videoDesenfoque.style.filter = 'blur(5px)'
+},10)
+
 });
 
 
+/* CIERRE MENU HAMB */
 const cierreX = document.getElementById('cierreX')
-
-
 cierreX.addEventListener('click', function () {
 const menuHamb = document.getElementById('menuHamb')
+const videoDesenfoque = document.getElementById('videoBlur')
 menuHamb.classList.remove('header__navHamb__ul--b')
 menuHamb.classList.add('header__navHamb__ul')
-console.log(menuHamb)
+setTimeout(function () {
+menuHamb.style.display = 'none'
+},1000)
+videoDesenfoque.style.filter = 'blur(0)'
 });
